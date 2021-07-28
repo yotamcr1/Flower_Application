d7948bdc20041d38adefbcd2c38b662da5894780
yotamcr1 <61942043+yotamcr1@users.noreply.github.com>
2021-07-28 18:43:45 +0300

    Create Convertey.py
    Added the code used to convert the model after the training process (from TF to TF_LITE)

---
+++ Data Set/Convertey.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+
+import os
+import signal
+import zipfile
+import datetime
+
+import matplotlib.pyplot as plt
+import matplotlib.image as mpimg
+
+import tensorflow as tf
+from tensorflow.keras import layers
+from tensorflow.keras import Model
+from tensorflow.keras.applications.inception_v3 import InceptionV3
+from tensorflow.keras.optimizers import RMSprop
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+from tensorflow.keras.optimizers import SGD
+
+from tensorflow import keras
+model = keras.models.load_model('./model')
+converter = tf.lite.TFLiteConverter.from_keras_model(model)
+tflite_model = converter.convert()
+
+# Save the model.
+with open('model.tflite', 'wb') as f:
+  f.write(tflite_model)
+
+
+
